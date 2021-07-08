cca4ce12e59f72b3c7478572868496f4fe42d718
TatiBarah <tatianabarahona57@gmail.com>
2021-07-08 14:36:09 -0600

    register

--- app.py
+++ app.py
@@ -1,67 +1,17 @@
-from flask import Flask, render_template
-from flask_cors import CORS, cross_origin
+from flask import Flask
+from routes.main_routes import MainRoutes
+
+from routes.register_routes import RegisterRoutes
+
+# from routes.logprocess_routes import LogProcessRoutes
+# from routes.dashboard_routes import DashboardRoutes
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config["CORS_HEADERS"] = "Content-Type"
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-@app.route("/inicio")
-def inicio():
-    return render_template("inicio.html")
-
-
-@app.route("/productos")
-def productos():
-    return render_template("productos.html")
-
-
-@app.route("/cita")
-def cita():
-    return render_template("cita.html")
-
-
-@app.route("/contacto")
-def contacto():
-    return render_template("contacto.html")
-
-
-@app.route("/registro")
-def registro():
-    return render_template("registro.html")
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-
-@app.route("/register")
-def register():
-    return render_template("registration.html")
-
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
-@app.route("/add_cli")
-def addcli():
-    return render_template("addcli.html")
-
-@app.route("/add_adm")
-def addadm():
-    return render_template("addadm.html")
+app.secret_key = "Bal1s2e3c4r5e6t7k8e9y0+"
+MainRoutes.configure_routes(app)
+RegisterRoutes.configure_routes(app)
+# LogProcessRoutes.configure_routes(app)
+# DashboardRoutes.configure_routes(app)
 
 
 if __name__ == "__main__":
---
+++ appViejo.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
+from routes.register_routes import RegisterRoutes
+
+app = Flask(__name__)
+# cors = CORS(app)
+# app.config["CORS_HEADERS"] = "Content-Type"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/inicio")
+def inicio():
+    return render_template("inicio.html")
+
+
+@app.route("/productos")
+def productos():
+    return render_template("productos.html")
+
+
+@app.route("/cita")
+def cita():
+    return render_template("cita.html")
+
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
+
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/add_cli")
+def addcli():
+    return render_template("addcli.html")
+
+
+@app.route("/add_adm")
+def addadm():
+    return render_template("addadm.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
